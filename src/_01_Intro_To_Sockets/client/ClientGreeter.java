ed602ac8c21ea3f9f4c8c181f51df6038f4806ed
League <league@iMac.attlocal.net>
2019-07-21 12:56:26 -0700

    ff
    ff

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -9,9 +9,14 @@ public class ClientGreeter {
 	  //1. Create a String for the ip address of the server. 
 	  // If you don't know how to find a computer's ip address, ask about ifconfig on linux/mac and ipconfig on windows.
       
+	   String IPad = "192.168.1.176.";
+	   
       //2. Create an integer for the server's port number
+	   
+	   int port = 8080;
       
       //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
+	   
     
     	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
  
