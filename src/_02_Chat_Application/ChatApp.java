7fb4f8256e29e3569ebea3d5746275a4bf63fed0
League Student <league@iMac-8.attlocal.net>
2019-08-11 12:56:54 -0700

    g
    g

--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,9 +1,47 @@
 package _02_Chat_Application;
 
-/*
- * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
- */
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import _00_Click_Chat.gui.ButtonClicker;
+import _00_Click_Chat.networking.Client;
+import _00_Click_Chat.networking.Server;
 
 public class ChatApp {
+	
+	ChatAppServer server;
+	ChatAppClient client;
+	
+	public static void main(String[] args) {
+		new ChatApp();
+	}
+	
+	public ChatApp(){
 
+		int response = JOptionPane.showConfirmDialog(null, "Would you like to host a connection?", "Buttons!", JOptionPane.YES_NO_OPTION);
+		if(response == JOptionPane.YES_OPTION){
+			server = new ChatAppServer(8080);
+			JOptionPane.showMessageDialog(null, "Server started at: " + server.getIPAddress() + "\nPort: " + server.getPort());
+			String message = JOptionPane.showInputDialog("Type your message");
+			
+			server.sendMessage(message);
+			
+			server.start();
+			
+		}else{
+			String ipStr = JOptionPane.showInputDialog("Enter the IP Address");
+			String prtStr = JOptionPane.showInputDialog("Enter the port number");
+			int port = Integer.parseInt(prtStr);
+			client = new ChatAppClient(ipStr, port);
+			
+			String message = JOptionPane.showInputDialog("Type your message");
+
+			client.sendMessage(message);
+	
+			client.start();
+		}
+	}
 }
+
+	
+
